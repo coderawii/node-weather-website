eb13ca5afc3d60837b99405dca4750280abb2919
unknown <coderawii@gmail.com>
2019-05-16 03:50:02 +0200

    Added some more things about forecast like temperatureMin, temperatureMax, windSpeed, humidity, visibility, pressure with through body.currently or body.daily.data[0]

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,11 @@
 const request = require('request')
 
+//? I GOAL: Add new data to forecast
+    //? 1. Update the forecast string to include new data
+    //? 2. Commit ur changes
+    //? 3. Push ur changes to GitHub and deploy to Heroku
+    //? 4. Test ur work in the live app 
+
 const forecast = (latitude, longitude, callback) => {
     const url = `https://api.darksky.net/forecast/cb97aeed68ba67e07ed049179d995616/${latitude},${longitude}?units=si`;
 
@@ -9,7 +15,14 @@ const forecast = (latitude, longitude, callback) => {
         } else if ( body.error) {
             callback('Nemoguce pronaci lokaciju!', undefined)
         } else {
-            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`);
+            callback(undefined, 
+                `${body.daily.data[0].summary} It is currently ${body.currently.temperature}℃ out. 
+                There is a ${body.currently.precipProbability}% chance of rain.
+                 For girl hair info, humidity is ${body.currently.humidity}%.
+                 Maximum temperature ${body.daily.data[0].temperatureMax}℃ and the minimun ${body.daily.data[0].temperatureMin}℃.
+                 Wind speed is ${body.currently.windSpeed}kph.
+                 Visibility is ${body.currently.visibility}km.
+                 Pressure ${body.currently.pressure}hPa`);
         }
     })
 }
